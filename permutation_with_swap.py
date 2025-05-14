340a96ab1561ad35da840f6ff539154b67846ffe
Valter Uotila <valter.uotila@gmail.com>
2025-05-14 09:14:38 +0300

    Add results

--- permutation_with_swap.py
+++ permutation_with_swap.py
@@ -7,7 +7,7 @@ from ansatz import Ansatz
 from efficient_symmetry_group import EfficientSymmetricGroup
 
 num_qubits = 13
-depth = 3
+depth = 1
 ansatz_id = 19
 
 ansatz = Ansatz(ansatz_id, num_qubits, depth)
@@ -20,23 +20,33 @@ def feature_map(features):
     for i, feature in enumerate(features):
         qml.RX(feature[0], wires=i)
         qml.RZ(feature[1], wires=i)
-
-@qml.qnode(dev)
-def full_circuit(params, features):
-    feature_map(features)
-    
-    if params_shape_two_qubit is None:
-        circuit(params)
-    else:
-        circuit(params[0], params[1])
-    
-    return qml.expval(qml.prod(*[qml.PauliZ(i) for i in range(num_qubits)]))
-
+        
+def insert_swaps(orbits):
+    for orbit in orbits:
+        # Loop over pairs of qubits
+        for i in range(len(orbit) - 1):
+            qml.SWAP(wires=[orbit[i] - 1, orbit[i+1] - 1])
+        
 
 def process_cycle(cycle):
-    rotations = cycle[2]
-    features = [(rotations[i], rotations[i + 1]) for i in range(1, len(rotations) + 1, 2)]
+    orbits = cycle[1]
+    features = 2*np.pi*np.random.rand(num_qubits, 2)
     res = []
+    
+    @qml.qnode(dev)
+    def full_circuit(params, features):
+        
+        feature_map(features)
+        insert_swaps(orbits)
+        
+        if params_shape_two_qubit is None:
+            circuit(params)
+        else:
+            circuit(params[0], params[1])
+            
+        return qml.expval(qml.prod(*[qml.PauliZ(i) for i in range(num_qubits)]))
+
+    
     for _ in range(200):
         if params_shape_two_qubit is None:
             params_single = np.random.rand(*params_shape_single_qubit)
@@ -53,7 +63,7 @@ if __name__ == '__main__':
     
     results = {}
     variances = {}
-    n = num_qubits * 2
+    n = num_qubits
     sym_group = EfficientSymmetricGroup(n)
 
     for k in range(1, int(np.ceil(n/2))):
@@ -61,14 +71,14 @@ if __name__ == '__main__':
         results[k] = []
         k_cycle_datasets = sym_group.generate_k_cycles_dataset(k=k, num_samples=1000)
         n_cpus = os.cpu_count()
-        with ProcessPoolExecutor(max_workers=int(n_cpus-2)) as executor:
+        with ProcessPoolExecutor(max_workers=n_cpus - 1) as executor:
             results[k] = list(executor.map(process_cycle, k_cycle_datasets))
     
     for k, values in results.items():
         print(k, "cycles variance is ", np.var(values))
         variances[k] = np.var(values)
     
-    file = f'results_depth_{depth}.json'
+    file = f'results_depth_{depth}_swap.json'
     if os.path.exists(file):
         with open(file, 'r') as f:
             existing_data = json.load(f)
