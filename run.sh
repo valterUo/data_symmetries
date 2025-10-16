ff5cae2442bd1747cd7c7e82f2673e417ffe4fdb
Uotila Valter <uotilav2@login4.triton.aalto.fi>
2025-10-16 20:30:17 +0300

    Updated entanglement results

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --array=1-95                    # 5 depths × 19 ansatz IDs = 95 jobs
 #SBATCH --cpus-per-task=8               # CPUs per run (tweak as needed)
 #SBATCH --mem=4G
-#SBATCH --time=24:00:00
+#SBATCH --time=10:00:00
 
 module load scicomp-python-env
 
@@ -17,5 +17,5 @@ DEPTH=$(( ($SLURM_ARRAY_TASK_ID - 1) % 5 + 1 ))
 ANSATZ_ID=$(( ($SLURM_ARRAY_TASK_ID - 1) % 19 + 1 ))
 
 echo "Running job with depth=$DEPTH, ansatz_id=$ANSATZ_ID"
-srun python expressibility.py --depth "$DEPTH" --ansatz_id "$ANSATZ_ID"
-#srun python entangling_capability.py --depth "$DEPTH" --ansatz_id "$ANSATZ_ID"
+#srun python expressibility.py --depth "$DEPTH" --ansatz_id "$ANSATZ_ID"
+srun python entangling_capability.py --depth "$DEPTH" --ansatz_id "$ANSATZ_ID"
